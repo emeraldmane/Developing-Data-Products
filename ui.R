702a931c74b35cd23de7d4574ea517d13c25c88c
Nathaniel Landi <emeraldmane@gmail.com>
2014-12-21 12:41:02 -0500

    Shiny

---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+# Define UI for iris application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Iris Dataset Analysis"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("variable", "First variable:",
+                list("Sepal length" = "Sepal.Length",
+                     "Sepal width"  = "Sepal.Width",
+                     "Petal length" = "Petal.Length",
+                     "Petal width"  = "Petal.Width")),
+    
+    selectInput("variable2", "Second variable:",
+                list("Sepal length" = "Sepal.Length",
+                     "Sepal width"  = "Sepal.Width",
+                     "Petal length" = "Petal.Length",
+                     "Petal width"  = "Petal.Width"))
+  ),
+  
+  mainPanel(
+    h3(textOutput("caption")),
+    plotOutput("plot")
+  )
+))
